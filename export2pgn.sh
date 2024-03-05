0e7a2b849b18638b6f250c0b4e2595f84e1b13b6
WWWishere <57116512+WWWishere@users.noreply.github.com>
2024-03-05 18:29:26 -0500

    style update

--- export2pgn.sh
+++ export2pgn.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export MONGO_HOST="mongodb://127.0.0.1:27017"
+export MONGO_HOST="mongodb+srv://ccvx01constellation:D2EQOkzTRzRl2htZ@pych.6hvaz7j.mongodb.net/?retryWrites=true&w=majority&appName=pych"
 
 python3 -X dev server/export2pgn.py
