e74e4ac8a2f4cf6b64204d6023f21e1276cc5c90
WWWishere <57116512+WWWishere@users.noreply.github.com>
2024-03-05 12:46:14 -0500

    style update

--- client/document.ts
+++ client/document.ts
@@ -106,6 +106,8 @@ export function changePieceCSS(assetUrl: string, family: string, cssFile: string
         case "spartan": cssLinkIndex += 22; break;
         case "mansindam": cssLinkIndex += 23; break;
         case "ataxx": cssLinkIndex += 24; break;
+        case "paradigm": cssLinkIndex += 25; break;
+        case "sandbox": cssLinkIndex += 26; break;
         default: throw "Unknown piece family " + family;
     }
     let newUrl = `${assetUrl}/piece-css/${family}/${cssFile}.css`;
